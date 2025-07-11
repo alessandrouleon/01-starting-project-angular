8654881cc5942db86027977e0d4362eabbbb84de
alessandro uleon <alessandrouleon.dev@gmail.com>
2025-07-10 22:12:37 -0400

    feat: add form to componets

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
