93582af431407a8f46770f382da5350132761887
alessandro uleon <alessandrouleon.dev@gmail.com>
2025-08-30 22:47:11 -0400

    feat: crete control componet

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+   title: string = '';
+   request: string = '';
 }
